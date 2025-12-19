131c8f722b14a955b62d2d989b41f4616038deca
lpiedras4 <piedras.leo@gmail.com>
2025-12-18 21:05:43 -0800

    Started stopwatch script

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 let date = new Date();
 document.getElementById("date-text").innerHTML="<h1>Today's date is " + date + "</h1>"
+let startTime;
+let elapsedTime = 0;
+let timerInterval;
+let isRunning = false;
+const start = document.getElementById("start-btn");
+const reset = document.getElementById("reset-btn");
