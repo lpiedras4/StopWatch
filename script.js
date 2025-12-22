8266ce4545fe5fac7ef9bb50ba0332d362d119c8
lpiedras4 <piedras.leo@gmail.com>
2025-12-21 21:32:25 -0800

    Fixed action listeners issue and implemented functions for pause and resume stopwatch

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-let date = new Date();
-document.getElementById("date-text").innerHTML="<h1>Today's date is " + date + "</h1>"
 let startTime; // exact timestamp when the user clicks "start"
 let elapsedTime = 0; // total time passed (for pause and resume funcitons)
 let timerInterval; //stores function to display every few miliseconds
-let isRunning = false;
+let isRunning = false; // bool to indicate when stopwatch is running
 let min = 0;
 let sec = 0;
 let centisec = 0;
@@ -12,46 +10,49 @@ const secBox = document.getElementById("sec-box");
 const centiSecondsBox = document.getElementById("centiseconds-box");
 const startButton = document.getElementById("start-btn");
 const resetButton = document.getElementById("reset-btn");
-startButton.addEventListener("click",start);
+startButton.addEventListener("click",toggleTimer);
 resetButton.addEventListener("click",reset);
 
-function start(){
-    
-    startTime = Date.now();
-    timerInterval = setInterval(function updateTime() {
-    const currTime = Date.now();
-    const timePassed = currTime - startTime + elapsedTime
-    updateDisplay(timePassed);
-    },10)
+function toggleTimer(){
+    if(!isRunning){
+    isRunning = true;
+    startTime = Date.now() - elapsedTime;
     startButton.textContent = "Pause";
-    if(this.textContent === "Pause"){
-        startButton.addEventListener("click",pause);
+    timerInterval = setInterval(function updateTime() {
+    elapsedTime= Date.now() - startTime;
+    updateDisplay(elapsedTime);
+    },10)
+    }else{
+        isRunning = false;
+        startButton.textContent="Resume";
+        clearInterval(timerInterval);
     }
+   
 }
 
 function updateDisplay(ms){
     //calculate time units
-    minutes = Math.floor(ms / 60000);
-    seconds = Math.floor ((ms % 60000) / 1000);
-    centiseconds = Math.floor ((ms % 1000) / 10);
+    min = Math.floor(ms / 60000);
+    sec = Math.floor ((ms % 60000) / 1000);
+    centisec = Math.floor ((ms % 1000) / 10);
     //format with leading ceros
-    const fMins = minutes.toString().padStart(2, "0");
-    const fSecs = seconds.toString().padStart(2, "0");
-    const fCentis = centiseconds.toString().padStart(2, "0");
+    const fMins = min.toString().padStart(2, "0");
+    const fSecs = sec.toString().padStart(2, "0");
+    const fCentis = centisec.toString().padStart(2, "0");
     minBox.childNodes[0].textContent = fMins + " ";
     secBox.childNodes[0].textContent = fSecs + " ";
     centiSecondsBox.childNodes[0].textContent = fCentis + " ";
 }
 function reset(){
+    isRunning = false;
     clearInterval(timerInterval);
     elapsedTime = 0;
     startButton.textContent = "Start";
-    updateDisplay(elapsedTime);
+    updateDisplay(0);
 }
 function pause(){
     startButton.textContent = "Resume";
     startButton.addEventListener("click",resume);
 }
-function resume(){
-    
-}
+let date = new Date();
+document.getElementById("date-text").innerHTML="<h1>Today's date is " + date + "</h1>"
